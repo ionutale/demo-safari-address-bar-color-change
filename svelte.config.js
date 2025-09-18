019ee4a257a3992762f398907c7972733893459e
Ion Utale <utale.ion@gmail.com>
2025-09-18 19:43:08 +0200

    fix: switch from static to node adapter in SvelteKit configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const isDev = process.env.NODE_ENV === 'development';
-
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: isDev ? '' : '/demo-safari-address-bar-color-change'
-		}
+		adapter: adapter()
 	}
 };
 
