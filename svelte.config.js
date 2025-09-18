fa69937bac6465fd3d8226117ea8e2b3e4a1dee5
Ion Utale <utale.ion@gmail.com>
2025-09-18 19:47:58 +0200

    chore: configure adapter-static + base for GitHub Pages (Actions deploy)

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+const isDev = process.env.NODE_ENV === 'development';
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: isDev ? '' : '/demo-safari-address-bar-color-change'
+		}
 	}
 };
 
